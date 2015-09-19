fb0939cf37d6bb748fa24f6c2a0d875f21ad6d38
Islam Wazery <wazery@ubuntu.com>
2015-09-19 22:53:01 +0200

    Change app name to HackHub and add new Digital Ocean server settings

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -3,10 +3,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/api-deployer/hacktoday-api/current
+APP_ROOT=/home/deployer/hackhub-api/current
 PID=$APP_ROOT/../shared/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=api-deployer
+AS_USER=deployer
 set -u
 
 OLD_PIN="$PID.oldbin"
