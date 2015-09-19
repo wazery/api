c8968c1d0cec9b5caa1b17b303663d3c4fcae6d6
Islam Wazery <wazery@ubuntu.com>
2015-09-19 22:57:24 +0200

    Modify RSpec settings slightly and omit Rubocop small offences

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails'
-# Pick the frameworks you want:
 require 'action_controller/railtie'
+require 'action_mailer/railtie'
 # require 'active_model/railtie'
 # require 'active_job/railtie'
 # require "active_record/railtie"
-require 'action_mailer/railtie'
-# require 'rails/test_unit/railtie'
-
-# require 'pp'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -29,6 +25,11 @@ module Api
     #    ::Moped::BSON = ::BSON
     #    AwesomePrint.pry!
     # end
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
