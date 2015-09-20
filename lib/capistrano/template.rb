ce62bc89b4fea4814b52be6475c59d99c0f5be9f
Islam Wazery <wazery@ubuntu.com>
2015-09-20 02:20:41 +0200

    Fix capistrano task small error

--- lib/capistrano/template.rb
+++ lib/capistrano/template.rb
@@ -13,7 +13,7 @@
 def smart_template(from, to = nil)
   to ||= from
   full_to_path = "#{shared_path}/config/#{to}"
-  if from_erb_path == template_file(from)
+  if from_erb_path = template_file(from)
     from_erb = StringIO.new(ERB.new(File.read(from_erb_path)).result(binding))
     upload! from_erb, full_to_path
     info "copying: #{from_erb} to: #{full_to_path}"
