74dd0e19a557b7bec73e01291f2d8115609f7bb1
Islam Wazery <wazery@ubuntu.com>
2015-09-24 01:10:13 +0200

    Add Chrome Github authentication endpoints

--- app/controllers/hackers_controller.rb
+++ app/controllers/hackers_controller.rb
@@ -25,4 +25,41 @@ class HackersController < ApplicationController
   rescue => e
     return render json: { message: e.message }, status: 401
   end
+
+  # Documentation
+  api :GET, '/hackers/auth', 'Authenticate hacker using Github OAuth'
+  description 'Redirects to the authorization URL'
+  param :code, String, required: true
+  # /Documentation
+  def auth
+    redirect_to github_auth_url
+  end
+
+  # Documentation
+  api :POST, '/hackers/callback', 'Authenticate hacker using an OAuth provider'
+  description ''
+  param :code, String, required: true
+  # /Documentation
+  def callback
+    hacker = Hacker.find_or_create_by_github_oauth_code(params[:code])
+
+    fail 'Authentication failed' unless hacker
+
+    redirect_to 'https://github.com'
+  end
+
+  private
+
+  def scopes
+    # TODO: Return different scopes depending on the auth phase
+    'user:email,repo'
+  end
+
+  def redirect_uri
+    "#{root_url}/auth/callback?agent=&redirect=https://github.com"
+  end
+
+  def github_auth_url
+    URI.encode("https://github.com/login/oauth/authorize?response_type=code&redirect_uri=#{redirect_uri}&scope=#{scopes}&client_id=#{Octokit.client_id}")
+  end
 end
