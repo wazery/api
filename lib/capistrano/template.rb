52c49f02b4af71c7a3bed06a5028c0f6953fe9ed
Islam Wazery <wazery@ubuntu.com>
2015-09-23 18:56:15 +0200

    Fix Rubocop offences

--- lib/capistrano/template.rb
+++ lib/capistrano/template.rb
@@ -13,7 +13,8 @@
 def smart_template(from, to = nil)
   to ||= from
   full_to_path = "#{shared_path}/config/#{to}"
-  if from_erb_path = template_file(from)
+  from_erb_path = template_file(from)
+  if from_erb_path
     from_erb = StringIO.new(ERB.new(File.read(from_erb_path)).result(binding))
     upload! from_erb, full_to_path
     info "copying: #{from_erb} to: #{full_to_path}"
