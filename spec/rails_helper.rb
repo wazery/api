5c2c85bb8d070abfc1074eee5546eb79555cf735
Islam Wazery <wazery@ubuntu.com>
2015-09-23 17:42:11 +0200

    Fix Mongoid configuration issue with RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
@@ -8,6 +9,7 @@ require 'rspec/rails'
 
 # Require support folder
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -41,8 +43,8 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  # Clean out test-database with Rspec and MongoID
-  config.use_transactional_fixtures = false
+  # Clean out test-database with Rspec and Mongoid
+  config.use_transactional_fixtures = true
 
   config.before :each do
     DatabaseCleaner.strategy = :truncation
